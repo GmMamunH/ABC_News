ccba2b4492c760aff450b45e308e8220057b8704
GmMamunH <gmmamun3004@gmail.com>
2025-01-04 17:23:43 +0600

    get(business): get business new using axios

--- src/pages/business/Business.jsx
+++ src/pages/business/Business.jsx
@@ -1,7 +1,34 @@
-
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const Business = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0cc82a8a00ef4b29b7c20b3705bb46e0"
+      )
+      .then(function (response) {
+        // handle success
+        console.log(response?.data?.articles);
+        setPosts(response?.data?.articles);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
   return (
-    <div>Business</div>
-  )
-}
+    <>
+      {posts.map((post) => (
+        <div key={post?.index}>
+          <p>{post?.content}</p>
+        </div>
+      ))}
+    </>
+  );
+};
